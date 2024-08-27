57296465fc4f9ef1fac56026c29c38c9ead2af4a
kjndfahu <denispoda59@gmail.com>
2024-08-27 23:35:19 +0300

    dsds

--- src/components/PortfolioSlider/PortfolioSlider.jsx
+++ src/components/PortfolioSlider/PortfolioSlider.jsx
@@ -15,7 +15,7 @@ export default function PortfolioSlider() {
         <div className="portfolio-slider">
             <Swiper
                 slidesPerView={5}
-                spaceBetween={150}
+                spaceBetween={30}
                 navigation={true}
                 centeredSlides={true}
                 loop={true}
--- src/components/TechStack/TechStack.jsx
+++ src/components/TechStack/TechStack.jsx
@@ -11,37 +11,37 @@ function TechStack() {
                 <div className={styles.laptop}><Laptop/></div>
                 <div className={styles.zeus}><Zeus/></div>
             </div>
-            <div className={styles.mainlineabs}>
-                <Vue/>
-                <Cpp/>
-                <Php/>
-                <Html/>
-                <Python/>
-                <Csharp/>
-                <NodeLogo/>
-                <Js/>
-                <Css/>
-                <ReactLogo/>
-                <Vue/>
-                <Ts/>
-                <NodeLogo/>
-                <Python/>
-            </div>
-            <div className={styles.backlineabs}>
-                <Python/>
-                <ReactLogo/>
-                <Js/>
-                <Ts/>
-                <Cpp/>
-                <Vue/>
-                <Php/>
-                <Python/>
-                <Csharp/>
-                <Php/>
-                <Js/>
-                <Html/>
-                <Vue/>
-            </div>
+            {/*<div className={styles.mainlineabs}>*/}
+            {/*    <Vue/>*/}
+            {/*    <Cpp/>*/}
+            {/*    <Php/>*/}
+            {/*    <Html/>*/}
+            {/*    <Python/>*/}
+            {/*    <Csharp/>*/}
+            {/*    <NodeLogo/>*/}
+            {/*    <Js/>*/}
+            {/*    <Css/>*/}
+            {/*    <ReactLogo/>*/}
+            {/*    <Vue/>*/}
+            {/*    <Ts/>*/}
+            {/*    <NodeLogo/>*/}
+            {/*    <Python/>*/}
+            {/*</div>*/}
+            {/*<div className={styles.backlineabs}>*/}
+            {/*    <Python/>*/}
+            {/*    <ReactLogo/>*/}
+            {/*    <Js/>*/}
+            {/*    <Ts/>*/}
+            {/*    <Cpp/>*/}
+            {/*    <Vue/>*/}
+            {/*    <Php/>*/}
+            {/*    <Python/>*/}
+            {/*    <Csharp/>*/}
+            {/*    <Php/>*/}
+            {/*    <Js/>*/}
+            {/*    <Html/>*/}
+            {/*    <Vue/>*/}
+            {/*</div>*/}
             <div className={styles.content}>
                 <div className={styles.texts}>
                     <h2 className={styles.maintext}>Используем <span>современные</span> средства разработки</h2>
